6370443cfa63574333226ba3fdaf70e622b24af6
guydegnol <gt@guydegnol.net>
2023-02-15 09:38:42 +0100

    Some changes

--- bulkhours/physu/__init__.py
+++ bulkhours/physu/__init__.py
@@ -12,7 +12,8 @@ def get_potential(exp=True, theo=True):
 
         r = np.linspace(0.2, 7, 200)
         u = V(r)
-        plt.plot(r, u, color="salmon", linewidth=2, linestyle="-")
+        if exp:
+            plt.plot(r, u, color="salmon", linewidth=2, linestyle="-")
 
         ax.spines["top"].set_visible(False)
         ax.spines["bottom"].set_visible(False)
@@ -24,23 +25,21 @@ def get_potential(exp=True, theo=True):
         ax.plot((-0.3, 7.56), (10.5, 10.5), color="black", linewidth=2)
         ax.text(7.3, 9.5, "x", fontname="default")
         ax.plot([0.02, 0.02], (-0.35, 16), color="black", linewidth=2)
-        ax.plot([0.09, 0.09], (-0.3, 14), color="xkcd:sky blue", linewidth=1.5)
-        ax.plot([0.09, 5], (-0.3, -0.3), color="xkcd:sky blue", linewidth=1.5)
-
-        ax.text(-0.3, -0.3, "-U", fontname="Comic Sans")
         ax.text(-0.3, 14, "U(x)", fontname="Comic Sans", rotation=90)
-
-        ax.plot([5, 5], (-0.3, 10.2), color="xkcd:sky blue", linewidth=1.5)
-        ax.plot([5, 7], (10.2, 10.2), color="xkcd:sky blue", linewidth=1.5)
-
         ax.text(-0.08, 10.2, "0", fontname="Comic Sans")
         ax.text(5, 11, "a", fontname="Comic Sans")
+        ax.text(-0.3, -0.3, "-U", fontname="Comic Sans")
 
-        plt.show()
+        if theo:
+            ax.plot([0.09, 0.09], (-0.3, 14), color="xkcd:sky blue", linewidth=1.5)
+            ax.plot([0.09, 5], (-0.3, -0.3), color="xkcd:sky blue", linewidth=1.5)
+            ax.plot([5, 5], (-0.3, 10.2), color="xkcd:sky blue", linewidth=1.5)
+            ax.plot([5, 7], (10.2, 10.2), color="xkcd:sky blue", linewidth=1.5)
+
         return fig
 
 
-def get_pdf(modes=[0, 1]):
+def get_pdf(modes=[0, 1], fontname="Comic Sans"):
     with plt.xkcd():
         fig, ax = plt.subplots(figsize=(8, 5))
 
@@ -49,23 +48,20 @@ def get_pdf(modes=[0, 1]):
         ax.set_xticks([])
         ax.set_yticks([])
 
-        args = [[0.62, 0.9], [0.0, -0.08]]
+        args = [[0.62, 0.9], [0.9, 0.5]]
 
-        colors = ["blue", "red", None]
-
+        colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
         x = np.linspace(0, 1)
 
         for i in modes:
-            ax.plot(x, np.sin(np.pi * x * (i + 1)) ** 2, c=colors[i])
-            ax.text(*args[i], f"n={i+1}", color=colors[i], fontname="Comic Sans")
-        # ax.plot(x, np.sin(np.pi*2*x)**2, c="red")
-        # ax.text(0.9, 0.5, 'n=2', color="red", fontname="Comic Sans")
+            ax.plot(x, np.sin(np.pi * x * (i + 1)) ** 2, c=colors[i], label=f"n={i+1}")
 
-        ax.text(0.0, -0.08, "0", fontname="Comic Sans")
-        ax.text(0.98, -0.08, "a", fontname="Comic Sans")
+        ax.text(0.0, -0.08, "0", fontname=fontname)
+        ax.text(0.98, -0.08, "a", fontname=fontname)
 
         ax.set_xlim([-0.0, 1.0])
         ax.set_ylim([-0.0, 1.1])
-        ax.set_xlabel("x", fontname="Comic Sans")
-        ax.set_ylabel("P(x)", fontname="Comic Sans")
+        ax.set_xlabel("x", fontname=fontname)
+        ax.set_ylabel("P(x)", fontname=fontname)
+        ax.legend(loc=2)
         return fig
