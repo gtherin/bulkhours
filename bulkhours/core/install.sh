fa6f5901cce6fdb7510eb2b106090dc39c3b36e9
guydegnol <gt@guydegnol.net>
2023-02-16 15:45:29 +0100

    Add an installer script

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -1,6 +1,5 @@
+
 cd /content
 rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git 2> /dev/null
 echo "git clone https://github.com/guydegnol/bulkhours.git"
-echo "egaezgaeg"
-echo "Username: $1"
-echo "egaezgaeg"
+echo "$1" > bulkhours/.safe
