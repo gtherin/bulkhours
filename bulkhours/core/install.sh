cfcf4472969c86cca1d3c0bfa4a12c3b56534ca2
guydegnol <gt@guydegnol.net>
2023-02-16 15:36:00 +0100

    Add an installer script

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -1,6 +1,5 @@
-ls
 cd /content
-rm -rf bulkhours && git clone https://github.com/guydegnol/bulkhours.git > /dev/null
+rm -rf bulkhours && git clone https://github.com/guydegnol/bulkhours.git 2> /dev/null
 echo "git clone https://github.com/guydegnol/bulkhours.git"
 echo $1
 export PASS_CODE=gzegzehze
