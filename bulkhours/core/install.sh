e11181a5ea458f55d6f0a01a52aa695c998206e1
guydegnol <gt@guydegnol.net>
2023-02-17 18:52:46 +0100

    Add ts flag

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -1,26 +1,31 @@
 
 
+
 if [ -d /content ];
 then
-    echo "RUN git clone https://github.com/guydegnol/bulkhours.git"
+    TS=date '+%H:%M:%S' -d "+1 hour"
+    echo "RUN git clone https://github.com/guydegnol/bulkhours.git [$TS]" 
     rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git > /dev/null 2>&1
     if [ "$3" = "econometrics" ]; then
-        echo "RUN pip install yfinance"
+        TS=date '+%H:%M:%S' -d "+1 hour"
+        echo "RUN pip install yfinance [$TS]"
         pip install yfinance > /dev/null 2>&1
-        echo "RUN pip install geopandas"
+        TS=date '+%H:%M:%S' -d "+1 hour"
+        echo "RUN pip install geopandas [$TS]"
         pip install geopandas > /dev/null 2>&1
     fi
 
     BULK_DIR="/content"
 else
-    echo "RUN ln bulkhours"
+    TS=date '+%H:%M:%S' -d "+1 hour"
+    echo "RUN ln bulkhours [$TS]"
     BULK_DIR="/home/guydegnol/projects"
 fi
 
 
 VERSION=`cat "bulkhours/bulkhours/__version__.py" | awk -F  '"' '{print $2}'`
-#echo "ENV BULK Helper cOURSe (version=$VERSION, login=$1 env=$3)"
-echo "ENV login $1, env $3)"
+TS=date '+%H:%M:%S' -d "+1 hour"
+echo "ENV login $1, env $3 [$TS $VERSION]"
 
 JSON_FMT='{"login":"%s","pass_code":"%s","env":"%s"}\n'
 printf "$JSON_FMT" "$1" "$2" "$3" > "$BULK_DIR/bulkhours/.safe"
