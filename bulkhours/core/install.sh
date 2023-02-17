e8a1395fa2007f6a074c6153fb2533f54dcac0ee
guydegnol <gt@guydegnol.net>
2023-02-17 14:44:53 +0100

    Add env to config file

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -4,10 +4,10 @@ if [ -d /content ];
 then
     echo "RUN git clone https://github.com/guydegnol/bulkhours.git"
     rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git 2> /dev/null
-    #if [ "$3" = "econometrics" ]; then
-    #    echo "RUN pip install yfinance"
-    #    pip install yfinance 2> /dev/null
-    #fi
+    if [ "$3" = "econometrics" ]; then
+        echo "RUN pip install yfinance"
+        pip install yfinance 2> /dev/null
+    fi
 
     BULK_DIR="/content"
 else
@@ -18,7 +18,7 @@ fi
 
 cat "$BULK_DIR/bulkhours/bulkhours/__version__.py"
 VERSION="None"
-echo "ENV BULK Helper cOURSe (version=$VERSION, login=$1 env=$3)")
+echo "ENV BULK Helper cOURSe (version=$VERSION, login=$1 env=$3)"
 
 JSON_FMT='{"login":"%s","pass_code":"%s","env":"%s"}\n'
 printf "$JSON_FMT" "$1" "$2" "$3" > "$BULK_DIR/bulkhours/.safe"
