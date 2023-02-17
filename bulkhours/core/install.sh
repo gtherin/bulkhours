26f89b8a0ff873f3c0718bb487a799b8f7e410c1
guydegnol <gt@guydegnol.net>
2023-02-17 14:03:11 +0100

    Add env to config file

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -4,12 +4,12 @@ if [ -d /content ];
 then
     rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git 2> /dev/null
     echo "RUN git clone https://github.com/guydegnol/bulkhours.git USER $1"
-    JSON_FMT='{"user":"%s","pass_code":"%s","package_dir":"%s"}\n'
-    printf "$JSON_FMT" "$1" "$2" "/content/bulkhours/" > /content/bulkhours/.safe
+    JSON_FMT='{"user":"%s","pass_code":"%s","env":"%s","package_dir":"%s"}\n'
+    printf "$JSON_FMT" "$1" "$2" "$3" "/content/bulkhours/" > /content/bulkhours/.safe
 else
     echo "RUN use local bulkhours USER $1"
-    JSON_FMT='{"user":"%s","pass_code":"%s","package_dir":"%s"}\n'
-    printf "$JSON_FMT" "$1" "$2" "/home/guydegnol/projects/bulkhours/" > /home/guydegnol/projects/bulkhours/.safe
+    JSON_FMT='{"user":"%s","pass_code":"%s","env":"%s","package_dir":"%s"}\n'
+    printf "$JSON_FMT" "$1" "$2" "$3" "/home/guydegnol/projects/bulkhours/" > /home/guydegnol/projects/bulkhours/.safe
 fi
 
 
