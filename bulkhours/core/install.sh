b5791f6c0e1687d07cfc1bed8862a6c33964419c
guydegnol <gt@guydegnol.net>
2023-03-04 14:43:20 +0100

    Add library depe

--- bulkhours/core/install.sh
+++ bulkhours/core/install.sh
@@ -1,10 +1,27 @@
 
 
 
-if [ "$4" != "ARANCINI" ]; then
+login="$1"
+passwd="$2"
+envi="$3"
+passse="$4"
+notebook="$5"
+packages="$6"
+
+# Check the install procedure
+if [ "$passse" != "ARANCINI" ]; then
     echo "Library bulkhours is no more available.";
     exit 0;
 fi
+
+# Set up the package directory
+if [ -d /content ];
+then
+    BULK_DIR="/content"
+else
+    BULK_DIR="/home/guydegnol/projects"
+fi
+
 
 if [ -d /content ];
 then
@@ -12,28 +29,30 @@ then
     echo "RUN git clone https://github.com/guydegnol/bulkhours.git [$TS]" 
     rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git > /dev/null 2>&1
     if [ "$3" = "econometrics" ]; then
-        TS=`date '+%H:%M:%S' -d "+1 hour"`
-        echo "RUN pip install yfinance [$TS]"
-        pip install yfinance > /dev/null 2>&1
-        pip install descartes > /dev/null 2>&1
-        TS=`date '+%H:%M:%S' -d "+1 hour"`
-        echo "RUN pip install geopandas [$TS]"
-        pip install geopandas > /dev/null 2>&1
-        echo "RUN pip install descartes [$TS]"
-        pip install descartes > /dev/null 2>&1
+        export IFS=","
+        #packages="yfinance,geopandas,descartes"
+        for package in $packages; do
+            TS=`date '+%H:%M:%S' -d "+1 hour"`
+            echo "RUN pip install $package [$TS]"
+            pip install $package > /dev/null 2>&1
+        done
     fi
 
-    BULK_DIR="/content"
 else
     TS=`date '+%H:%M:%S' -d "+1 hour"`
     echo "RUN ln bulkhours [$TS]"
-    BULK_DIR="/home/guydegnol/projects"
+    export IFS=","
+    for package in $packages; do
+        TS=`date '+%H:%M:%S' -d "+1 hour"`
+        echo "RUN pip install $package [$TS]"
+    done
+
 fi
 
 
 VERSION=`cat "$BULK_DIR/bulkhours/bulkhours/__version__.py" | awk -F  '"' '{print $2}'`
 TS=`date '+%H:%M:%S' -d "+1 hour"`
-echo "ENV login $1, env $3 [$TS $VERSION]"
+echo "ENV login $login, env $envi [$TS $VERSION]"
 
 JSON_FMT='{"login":"%s","pass_code":"%s","env":"%s","course_version":"%s"}\n'
-printf "$JSON_FMT" "$1" "$2" "$3" "$4" > "$BULK_DIR/bulkhours/.safe"
+printf "$JSON_FMT" "$login" "$passwd" "$envi" "$passse" > "$BULK_DIR/bulkhours/.safe"
