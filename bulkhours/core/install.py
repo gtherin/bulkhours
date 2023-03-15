994653bf81ccc1658228cc67758a92a14356f5b2
guydegnol <gt@guydegnol.net>
2023-03-15 19:33:55 +0100

    Change script to python

--- bulkhours/core/install.py
+++ bulkhours/core/install.py
@@ -1,5 +1,9 @@
 import argparse
+import datetime
+import os
 import sys
+import time
+import json
 
 
 def get_arg_parser(argv):
@@ -7,84 +11,58 @@ def get_arg_parser(argv):
         description="Installation script evaluation",
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("-e", "--evaluation-id", help=f"Select which correction to apply")
-    parser.add_argument("-s", "--show-solution", help="Show the solution", action="store_true")
-    parser.add_argument("-d", "--delete-solution", help="Delete the solution", action="store_true")
-    parser.add_argument("-c", "--pass-code", help="Pass code")
-    # parser.add_argument("-p", "--promo", help="Promo", default=promotion.current_promo)
-
-    parser = argparse.ArgumentParser(description="Evaluation params")
-    parser.add_argument("-i", "--id", default=None)
     parser.add_argument("-u", "--user", default=os.environ["STUDENT"])
-    parser.add_argument("-o", "--options", default=None)
+    parser.add_argument("-x", "--pass-code", help="Pass code", default=None)
+    parser.add_argument("-X", "--pass-phrase", help="Pass code", default=None)
+    parser.add_argument("-e", "--env-id", help=f"Environnment id")
+    parser.add_argument("-i", "--id", default=None)
+    parser.add_argument("-p", "--packages", default="")
 
     return parser.parse_args(argv)
 
 
 def main(argv=sys.argv[1:]):
-    print("FFFFFFFFFFFFFFFF")
+    args = get_arg_parser(argv)
+
+    # Set up a colab flag
+    is_colab = os.path.exists("/content")
+
+    if args.pass_phrase != "ARANCINI":
+        print("RUN install bulkhours: aborted 💥")
+        return
+
+    # Log datetime
+    stime = datetime.datetime.now() + datetime.timedelta(seconds=3600) if is_colab else datetime.datetime.now()
+    print("RUN install bulkhours [%s]" % stime.strftime("%H:%M:%S"))
+
+    # Set up the package directory
+    bulk_dir = "/content" if is_colab else "/home/guydegnol/projects"
+    env_id = "mock" if is_colab else "colab"
+
+    # Install main package
+    start_time = time.time()
+    print("RUN git clone https://github.com/guydegnol/bulkhours.git [%s, %.0fs]" % (env_id, time.time() - start_time))
+    if is_colab:
+        os.system(
+            f"cd {bulk_dir} && rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git --depth 1 > /dev/null 2>&1"
+        )
+
+    # Update pip
+    start_time = time.time()
+    print("RUN pip install --upgrade pip [%s, %.0fs]" % (env_id, time.time() - start_time))
+    if is_colab:
+        os.system(f"pip install --upgrade pip > /dev/null 2>&1")
+
+    # Install packages
+    for package in args.packages.split(","):
+        start_time = time.time()
+        print("RUN pip install %s [%s, %.0fs]" % (package, env_id, time.time() - start_time))
+        os.system(f"pip install {package} > /dev/null 2>&1")
+
+    data = {"login": args.user, "pass_code": args.pass_code, "env": args.env_id, "course_version": args.pass_phrase}
+    with open(f"{bulk_dir}/bulkhours/.safe", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
     main()
-
-
-"""
-login="$1"
-passwd="$2"
-envi="$3"
-passse="$4"
-notebook="$5"
-packages="$6"
-
-# Check the install procedure
-if [ "$passse" != "ARANCINI" ]; then
-    echo "Library bulkhours is no more available.";
-    exit 0;
-fi
-
-# Set up the package directory
-if [ -d /content ];
-then
-    BULK_DIR="/content"
-else
-    BULK_DIR="/home/guydegnol/projects"
-fi
-
-
-if [ -d /content ];
-then
-    TS=`date '+%H:%M:%S' -d "+1 hour"`
-    echo "RUN git clone https://github.com/guydegnol/bulkhours.git [$TS]" 
-    rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git --depth 1 > /dev/null 2>&1
-    echo "RUN pip install --upgrade pip [$TS]"
-    pip install --upgrade pip > /dev/null 2>&1
-    if [ "$3" = "econometrics" ]; then
-        export IFS=","
-        #packages="yfinance,geopandas,descartes"
-        for package in $packages; do
-            TS=`date '+%H:%M:%S' -d "+1 hour"`
-            echo "RUN pip install $package [$TS]"
-            pip install $package > /dev/null 2>&1
-        done
-    fi
-
-else
-    TS=`date '+%H:%M:%S' -d "+1 hour"`
-    echo "RUN ln bulkhours [$TS]"
-    export IFS=","
-    for package in $packages; do
-        TS=`date '+%H:%M:%S' -d "+1 hour"`
-        echo "RUN pip install $package [$TS]"
-    done
-
-fi
-
-
-VERSION=`cat "$BULK_DIR/bulkhours/bulkhours/__version__.py" | awk -F  '"' '{print $2}'`
-TS=`date '+%H:%M:%S' -d "+1 hour"`
-echo "ENV login $login, env $envi [$TS $VERSION]"
-
-JSON_FMT='{"login":"%s","pass_code":"%s","env":"%s","course_version":"%s"}\n'
-printf "$JSON_FMT" "$login" "$passwd" "$envi" "$passse" > "$BULK_DIR/bulkhours/.safe"
-"""
