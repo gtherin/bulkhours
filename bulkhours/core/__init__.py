85a5e512033c833c2fab23ab19cdec5ff3b580ce
guydegnol <gt@guydegnol.net>
2023-04-13 14:14:17 +0200

    Fix execution

--- bulkhours/core/__init__.py
+++ bulkhours/core/__init__.py
@@ -32,6 +32,19 @@ def git_push(argv=sys.argv[1:]):
         the_file.write(f"""__version__ = "{nversion}"\n""")
 
     print(f"Update {oversion} => {nversion}")
+    with open("git_push.sh", "w") as f:
+        f.write(
+            f"""git pull
+git status && git add . && git commit -m "{args.message}"
+git push
+"""
+        )
+
+    print("AAAAAAAAAAA")
+    print(
+        subprocess.run("bash git_push.sh".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
+    )
+
     cmd = f"""git add . && git commit -m "{args.message}" && git push"""
     Popen(cmd, stdout=PIPE, shell=True, stderr=STDOUT)
 
