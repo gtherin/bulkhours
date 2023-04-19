72f73da2ed4f3049150e8c4417a6e979832d1280
guydegnol <gt@guydegnol.net>
2023-04-19 18:19:30 +0200

    Add better comments

--- bulkhours/core/firebase.py
+++ bulkhours/core/firebase.py
@@ -17,6 +17,13 @@ def send_answer_to_corrector(cinfo, update=True, user=None, comment="", update_t
     question = cinfo.id
     user = os.environ["STUDENT"] if user is None else user
 
+    corr = get_solution_from_corrector(question, corrector="solution")
+    if corr is not None:
+        print(
+            f"\x1b[31m\x1b[1m The answer can not be submitted. Solution for {question} has already been submitted.  \x1b[m"
+        )
+        return
+
     uptime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if update_time:
         kwargs.update({"update_time": uptime})
@@ -27,7 +34,9 @@ def send_answer_to_corrector(cinfo, update=True, user=None, comment="", update_t
         get_document(question, user).update(kwargs)
     else:
         get_document(question, user).set(kwargs)
-    print(f"Answer {comment} has been submited at {uptime} for {question}/{user}. You can resubmit it several times")
+    print(
+        f"\x1b[32m\x1b[1m Answer {comment} has been submited at {uptime} for {question}/{user}. You can resubmit it several times \x1b[m"
+    )
 
 
 def get_solution_from_corrector(question, corrector="solution"):
