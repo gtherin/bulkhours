547091979b4cca776ed918620d8bfa3794841f28
guydegnol <gt@guydegnol.net>
2023-05-19 15:36:40 +0200

    Tune install script

--- bulkhours/__init__.py
+++ bulkhours/__init__.py
@@ -164,21 +164,6 @@ def geo_plot(data=None, timeopt="last", **kwargs):
     return geo.geo_plot(data, timeopt=timeopt, **kwargs)
 
 
-"""
-def get_config(directory=None):
-    import json
-
-    directory = os.path.dirname(__file__) + "/../" if directory is None else directory
-
-    jsonfile = directory + "/.safe"
-    if os.path.exists(jsonfile):
-        with open(jsonfile) as json_file:
-            return json.load(json_file)
-
-    return {}
-"""
-
-
 def init(verbose=False):
     kwargs = get_config()
     if len(kwargs) > 1:
--- bulkhours/__version__.py
+++ bulkhours/__version__.py
@@ -1,3 +1,3 @@
-__version__ = "3.4.34"
-__aversion__ = "2.6.34"
-__mversion__ = "1.1.34"
+__version__ = "3.4.35"
+__aversion__ = "2.6.35"
+__mversion__ = "1.1.35"
