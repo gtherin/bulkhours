30e83bb3c3507c692979f6282b5bece8da903709
guydegnol <bulkhours@guydegnol.net>
2023-06-22 21:11:06 +0200

    Better readme

--- bulkhours/__version__.py
+++ bulkhours/__version__.py
@@ -1,3 +1,3 @@
-__version__ = "5.1.17"
-__aversion__ = "3.8.6"
-__mversion__ = "2.8.6"
+__version__ = "5.1.18"
+__aversion__ = "3.8.7"
+__mversion__ = "2.8.7"
--- bulkhours/data/help.py
+++ bulkhours/data/help.py
@@ -25,11 +25,75 @@ def get_rdata(d, dname):
     return f"[{d[dname]}](https://github.com/guydegnol/bulkhours/blob/main/data/{d[dname]})"
 
 
+class Script:
+    def __init__(self, text="", fname="script.sh") -> None:
+        self.text = text + "\n"
+        self.fname = fname
+
+    def add_line(self, l) -> None:
+        self.text += l + "\n"
+
+    def execute(self, verbose=False) -> None:
+        import subprocess
+
+        if verbose:
+            print(self.text)
+        with open(self.fname, "w") as f:
+            f.write(self.text)
+
+        print(
+            subprocess.run(
+                f"bash {self.fname}".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+            ).stdout
+        )
+
+        os.system(f"rm -rf {self.fname}")
+
+
 def build_readme(load_data=True):
-    ffile = open(get_readme_filename(), "w")
-    ffile.write("# Data\n\n")
+    if 0:
+        s = Script("cd /home/guydegnol/projects/bulkhours")
+        tdir = "../bulkhours.wiki"
+        s.add_line(f"/opt/miniconda/condabin/conda activate bulkhours_py3.10")
+        s.add_line(f"cp -r README.md {tdir}/Home.md")
+        s.add_line(f"cp -r data/README.md {tdir}/Data.md")
+        s.add_line(f"cp -r bulkhours/ecox/README.md {tdir}/Econometrics.md")
+        s.add_line(f"cp -r bulkhours/hpc/README.md {tdir}/HPC.md")
+        s.add_line(f"/opt/miniconda/envs/bulkhours_py3.10/bin/pdoc bulkhours/core/tools.py --force -o {tdir}/docs")
+        s.add_line(
+            f"/opt/miniconda/envs/bulkhours_py3.10/bin/pdoc ../bulkhours_premium/bulkhours_premium/equals.py -o {tdir}/docs"
+        )
+        s.execute(verbose=True)
+
+    import pdoc
+
+    # import markdown2
+
+    modules = ["bulkhours_premium.equals"]
+    context = pdoc.Context()  # docformat="numpy")  # markdown restructuredtext google numpy
+
+    modules = [pdoc.Module(mod, context=context) for mod in modules]
+
+    pdoc.link_inheritance(context)
+
+    def recursive_htmls(mod):
+        yield mod.name, mod.text()  # text html
+        for submod in mod.submodules():
+            print(submod)
+            yield from recursive_htmls(submod)
+
+    for mod in modules:
+        for module_name, html in recursive_htmls(mod):
+            with open(f"/home/guydegnol/projects/bulkhours.wiki/{module_name.replace('.', '_')}.md", "w") as ff:
+                ff.write(html)
+                print(html)
+            # print(module_name, html)
+    return
 
     from ..phyu.constants import Units
+
+    ffile = open(get_readme_filename(), "w")
+    ffile.write("# Data\n\n")
 
     for c, category in enumerate(datacategories):
         ffile.write(f'- [{c+1}. {category["label"]}](#{category["tag"]}) \n')
