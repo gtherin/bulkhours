7397cf2b6504d4500e258ab59f73b31ac1d0f866
guydegnol <bulkhours@guydegnol.net>
2023-06-27 14:42:50 +0200

    Restauration time

--- bulkhours/core/__init__.py
+++ bulkhours/core/__init__.py
@@ -24,34 +24,27 @@ def git_push(argv=sys.argv[1:]):
         else:
             return f"{sversion[0]}.{sversion[1]}.{int(sversion[2])+1}"
 
-    def get_fversion(v):
-        root_dir = "/home/guydegnol/projects"
-        return f"{root_dir}/bulkhours{v}/bulkhours{v}/__version__.py"
-
     # Update the data doc
     from bulkhours.data import build_readme
-
-    root_dir = "/home/guydegnol/projects"
 
     parser = argparse.ArgumentParser(description="Git helper")
     parser.add_argument("-m", "--message", help="Message", default="Some changes")
     parser.add_argument("-r", "--readme", action="store_true")
     args = parser.parse_args(argv)
 
-    if args.readme:
-        build_readme()
+    # if args.readme:
+    #    build_readme()
 
-    ps = {"": ""}
-    ovs = {k: open(get_fversion(v)).readline().split('"')[1] for k, v in ps.items()}
-    nvs = {k: get_nversion(v) for k, v in ovs.items()}
+    root_dir = "/home/guydegnol/projects"
+    ovs = open(filename := f"{root_dir}/bulkhours/bulkhours/__version__.py").readline().split('"')[1]
+    nvs = get_nversion(ovs)
 
-    for r, p in ps.items():
-        with open(get_fversion(p), "w") as the_file:
-            the_file.write(f"""__version__ = "{nvs['']}"\n""")
+    with open(filename, "w") as the_file:
+        the_file.write(f"""__version__ = "{nvs}"\n""")
 
     with open("git_push.sh", "w") as f:
         f.write(f"""python {root_dir}/pyservice/pyservice/generate_bulkhours_keys.py\n""")
-        for r, p in ps.items():
+        for p in [""]:  # , ".wiki"]:
             f.write(
                 f"""cd ../bulkhours{p} && git pull && git add . && git commit -m "{args.message}" && git push 2> /dev/null\n"""
             )
@@ -62,6 +55,4 @@ def git_push(argv=sys.argv[1:]):
 
     os.system("rm -rf git_push.sh")
 
-    print(
-        f"BULK Helper cOURSe: \x1b[0mversion='{ovs['']}=>{nvs['']}' \x1b[36mpversion='{ovs['m']}=>{nvs['m']}'\x1b[0m🚀, \x1b[31maversion='{ovs['a']}=>{nvs['a']}'\x1b[0m⚠️"
-    )
+    print(f"BULK Helper cOURSe: \x1b[36mpversion='{ovs}=>{nvs}'\x1b[0m🚀")
