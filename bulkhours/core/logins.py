10118e3591a0d8f9b48c16ac25ea46a073fad670
guydegnol <bulkhours@guydegnol.net>
2023-07-04 10:50:21 +0200

    Update data

--- bulkhours/core/logins.py
+++ bulkhours/core/logins.py
@@ -133,6 +133,7 @@ import ipywidgets
         )
 
     print("\n" + info)
+    print("AAAAAAAAAAA", tools.get_value("openai_token"))
     if tools.get_value("openai_token") is not None:
         if ipp := IPython.get_ipython():
             ipp.run_cell(
