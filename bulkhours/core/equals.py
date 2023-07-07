a74a79e1ede5ac43fa12689823f76a4b9bb26692
guydegnol <bulkhours@guydegnol.net>
2023-07-07 18:58:35 +0200

    More robust

--- bulkhours/core/equals.py
+++ bulkhours/core/equals.py
@@ -1,11 +1,15 @@
 import IPython
 import difflib
-from . import contexts
 import numpy as np
 import pandas as pd
+import os
+
+from . import contexts
 
 
-def is_equal(data_test, data_ref, norm="Linf-norm", error=1e-8, policy="strict", min_score=0, max_score=10):
+def is_equal(
+    data_test, data_ref, norm="Linf-norm", error=1e-8, policy="strict", min_score=0, max_score=10, cmax_score=False
+):
     """Return the student score compared to a benchmark value
 
     Parameters:
@@ -35,12 +39,14 @@ def is_equal(data_test, data_ref, norm="Linf-norm", error=1e-8, policy="strict",
     return bulkhours.is_equal(data_test, data_ref=3, max_score=5, policy="gaussian", error=1e-8)
     """
 
+    if cmax_score:
+        return max_score
+
     if type(data_test) != type(data_ref):
-        print(f"\nFINAL_SCORE={min_score}/{max_score}\n")
         return min_score
 
     # Get error from student
-    if type(data_test) == list:
+    if type(data_test) in [list, tuple]:
         data_test = np.array(data_test)
         data_ref = np.array(data_ref)
 
@@ -67,7 +73,6 @@ def is_equal(data_test, data_ref, norm="Linf-norm", error=1e-8, policy="strict",
         score = np.clip(float(distance < error), 0, 1)
 
     final_score = np.round(score * (max_score - min_score) + min_score, 1)
-    print(f"\nFINAL_SCORE={final_score}/{max_score}\n")
     return final_score
 
 
@@ -95,32 +100,6 @@ def explain_student(student_data, teacher_data, raw=False):
         IPython.get_ipython().run_cell(explanation_code)
 
 
-def aggregate_notes(teacher_data, evaluation_code, scores):
-    """
-    This is an example of Google style.
-
-    Args:
-        scores: This is the first param.
-
-    Returns:
-        This is a description of what is returned.
-    """
-    agg_evaluation_code, counter = "", 0
-    for l in evaluation_code.splitlines():
-        if ".is_equal" in l:
-            l = l[: l.rfind("bulkhours.is_equal")] + scores[counter][: scores[counter].find("/")]
-            counter += 1
-        agg_evaluation_code += l + "\n"
-
-    agg_evaluation_code = agg_evaluation_code.replace("student_evaluation_function", "do_evaluate_student")
-    agg_evaluation_code += "print(f'FINAL_SCORE={r}/%s')\n" % teacher_data.max_score
-
-    if "show_code=true" in teacher_data.get_code("evaluation").replace(" ", "").lower():
-        print(agg_evaluation_code)
-    out = contexts.exec_code(agg_evaluation_code, False)
-    return [a.replace("FINAL_SCORE=", "") for a in out if "FINAL_SCORE=" in a][0].replace(")", "")
-
-
 def evaluate_student(student_data, teacher_data, raw=False, use_student_context=True):
     """
     This is a reST style.
@@ -129,7 +108,14 @@ def evaluate_student(student_data, teacher_data, raw=False, use_student_context=
     :returns: this is a description of what is returned
     """
     # Get the formatted evaluation code
-    evaluation_code = teacher_data.get_code("evaluation") + "\nr = student_evaluation_function()\n"
+    evaluation_code = (
+        teacher_data.get_code("evaluation")
+        + """global eresult
+eresult = student_evaluation_function()
+import os
+os.environ['FINAL_SCORE'] = str(eresult)
+"""
+    )
 
     do_debug = "debug=true" in evaluation_code.replace(" ", "").lower()
 
@@ -153,16 +139,17 @@ def evaluate_student(student_data, teacher_data, raw=False, use_student_context=
     if "show_code=true" in teacher_data.get_code("evaluation").replace(" ", "").lower():
         print(evaluation_code)
 
-    out = contexts.exec_code(evaluation_code, do_debug)
-    scores = [a.replace("FINAL_SCORE=", "") for a in out if "FINAL_SCORE=" in a]
-    if do_debug:
-        print("SCORES:", scores)
+    contexts.exec_code(evaluation_code, do_debug)
 
-    score = aggregate_notes(teacher_data, evaluation_code, scores)
+    score = float(os.environ["FINAL_SCORE"])
+
     if do_debug:
         print("SCORE:", score)
 
     if raw:
-        return float(score[: score.find("/")])
+        return score
 
-    return score
+    contexts.exec_code(evaluation_code.replace("student.", "teacher."), do_debug)
+    max_score = float(os.environ["FINAL_SCORE"])
+
+    return f"{score}/{max_score}"
