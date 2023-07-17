dc84cf18a734b518fddb0a7e8af6f0f96ad5aa65
guydegnol <bulkhours@guydegnol.net>
2023-07-17 14:14:22 +0200

    Add showarray

--- bulkhours/ml/lxmert/__init__.py
+++ bulkhours/ml/lxmert/__init__.py
@@ -7,3 +7,16 @@ try:
     from . import utils  # noqa
 except ImportError:
     print("torch is not installed")
+
+
+# for visualizing output
+def showarray(a, fmt="jpeg"):
+    import PIL.Image
+    import io
+    import numpy as np
+    from IPython.display import Image, display
+
+    a = np.uint8(np.clip(a, 0, 255))
+    f = io.BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    display(Image(data=f.getvalue()))
