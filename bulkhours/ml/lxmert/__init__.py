3cd1fbd8a8d86ad9616b61b4542ab0114b0b1c58
guydegnol <bulkhours@guydegnol.net>
2023-07-17 19:21:50 +0200

    Better system

--- bulkhours/ml/lxmert/__init__.py
+++ bulkhours/ml/lxmert/__init__.py
@@ -1,12 +1,13 @@
-try:
+# try:
+if 1:
     import torch
     from .processing_image import Preprocess  # noqa
     from .visualizing_image import SingleImageViz  # noqa
     from .modeling_frcnn import GeneralizedRCNN  # noqa
     from .utils import Config  # noqa
     from . import utils  # noqa
-except ImportError:
-    print("torch is not installed")
+# except ImportError:
+#    print("torch is not installed")
 
 
 # for visualizing output
