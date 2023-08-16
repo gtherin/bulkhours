ff4d474d0066a3c1347f0185ced02bb7b85a0924
guydegnol <bulkhours@guydegnol.net>
2023-08-16 13:56:26 +0200

    Adapt config for SageMaker

--- bulkhours/core/installer.py
+++ bulkhours/core/installer.py
@@ -1,17 +1,13 @@
-import argparse
 import os
-import sys
 import time
-import json
-
 import zlib
 from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+from .tools import get_platform
 
 DEFAULT_TOKEN = "NO_TOKEN"
-is_colab = os.path.exists("/content")
 
 # Set up the package directory
-bulk_dir = "/content" if is_colab else "/home/guydegnol/projects"
+bulk_dir = os.path.abspath(os.path.dirname(__file__) + f"/../../..")
 
 
 def obscure(data) -> bytes:
@@ -29,89 +25,29 @@ def get_tokens(token, raise_error=False, verbose=True):
         return {s[0]: s[1] for s in tokens}
 
     nb_helper, nb_key = token.split("::")
-    with open(f"{bulk_dir}/bulkhours/data/radian2.png") as f:
-        TOKENS = f.readline()
+    import glob
 
-    for db_key in TOKENS.split("::"):
-        ali, baba = db_key.split("__RR__")
-        if nb_helper == ali:
-            tokens = nb_key.encode("utf-8") + baba.encode("utf-8")
-            if raise_error:
-                return unobscure_token(tokens)
-            else:
-                try:
-                    return unobscure_token(tokens)
-                except:
-                    pass
+    for filename in glob.glob(f"{bulk_dir}/bulkhours/data/radian*.png"):
+        with open(filename) as f:
+            TOKENS = f.readline()
+
+            for db_key in TOKENS.split("::"):
+                ali, baba = db_key.split("__RR__")
+                if nb_helper == ali:
+                    tokens = nb_key.encode("utf-8") + baba.encode("utf-8")
+                    if raise_error:
+                        return unobscure_token(tokens)
+                    else:
+                        try:
+                            return unobscure_token(tokens)
+                        except:
+                            pass
     if verbose:
         print(
             f"""⚠️\x1b[41m\x1b[37mYour token was not found. Check that your token is still valid (contact: bulkhours@guydegnol.net)\x1b[0m⚠️"""
         )
 
     return {}
-
-
-def format_opt(label, raw2norm=True):
-    rr = {"-": "__minus__", "@": "__at__", " ": "__space__", "/": "__slash__"}
-    if len(label) > 0 and label[0] != "-":
-        for k, v in rr.items():
-            label = label.replace(k, v) if raw2norm else label.replace(v, k)
-    return label
-
-
-def format_opts(argv):
-    nargv = []
-    for a in argv:
-        if a[0] != "-" and nargv[-1][0] != "-":
-            nargv[-1] += " " + a
-        else:
-            nargv.append(a)
-    return [format_opt(a) for a in nargv]
-
-
-def get_install_parser(argv):
-    parser = argparse.ArgumentParser(
-        description="Installation script", formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    parser.add_argument("-p", "--packages", default="None")
-    parser.add_argument("-k", "--openai-token", default=DEFAULT_TOKEN)
-    parser.add_argument("-t", "--tokens", default={})
-    parser.add_argument("-d", "--debug", action="store_true")
-
-    argv = parser.parse_args(format_opts(argv))
-    for k in ["packages", "openai_token", "tokens"]:
-        if getattr(argv, k):
-            setattr(argv, k, format_opt(getattr(argv, k), raw2norm=False))
-
-    argv.tokens = get_tokens(argv.tokens)
-
-    return argv
-
-
-def install_pkg(pkg, is_colab, tokens, env_id, start_time, debug):
-    if not (f"{pkg}_token" in tokens and (token := tokens[f"{pkg}_token"]) != DEFAULT_TOKEN):
-        return
-    if is_colab:
-        os.system(
-            f"cd {bulk_dir} && rm -rf bulkhours_{pkg} 2> /dev/null && git clone https://{token.replace('/', '_')}@github.com/guydegnol/bulkhours_{pkg}.git --depth 1 > /dev/null 2>&1"
-        )
-    if debug:
-        print(f"git clone https://{token.replace('/', '_')}@github.com/guydegnol/bulkhours_{pkg}.git --depth 1")
-
-    if os.path.exists(f"{bulk_dir}/bulkhours_{pkg}/"):
-        if 0:
-            einfo = "🚀" if pkg != "admin" else "⚠️\x1b[41m\x1b[37mfor teachers only\x1b[0m"
-            color = "\x1b[36mRUN git clone " if pkg != "admin" else "\x1b[31mRUN git clone "
-
-            print(
-                "%shttps://github.com/guydegnol/bulkhours_%s.git [%s, %.0fs]\x1b[0m%s"
-                % (color, pkg, env_id, time.time() - start_time, einfo)
-            )
-
-    else:
-        print(
-            f"RUN install bulkhours_{pkg}: installation failed 🚫. Check that your {pkg}_token is still valid (contact: bulkhours@guydegnol.net)"
-        )
 
 
 def install_dependencies(packages, start_time):
@@ -121,10 +57,17 @@ def install_dependencies(packages, start_time):
     if start_time is None:
         start_time = time.time()
 
-    env_id = "colab" if is_colab else "mock"
+    if (
+        get_platform() == "sagemaker"
+        and "CONDA_PREFIX" in os.environ
+        and "sagemaker-distribution" not in os.environ["CONDA_PREFIX"]
+    ):
+        print(
+            f"""⚠️\x1b[33mFor sagemaker, please use the \033[1msagemaker-distribution:Python\x1b[0m\x1b[33m kernel (data science libraries are already installed) \x1b[0m"""
+        )
 
     # Update pip
-    print("\x1b[37mRUN pip/apt install [%s]: " % (env_id), end="", flush=True)
+    print("\x1b[37mRUN pip/apt install [%s]: " % (get_platform()), end="", flush=True)
 
     # packages = "swig,cmake,python-opengl,ffmpeg,xvfb,gym==0.25.2,pyvirtualdisplay,stable-baselines3[extra],box2d,box2d-kengz,array2gif,huggingface_sb3,pyglet==1.5.1"
     packages = packages.replace(
@@ -136,7 +79,7 @@ def install_dependencies(packages, start_time):
     # Install packages
     for package in packages.split(","):
         print("%s [%.0fs]," % (package, time.time() - start_time), end="", flush=True)
-        if not is_colab:
+        if get_platform() == "local":
             continue
 
         if package == "pip":
@@ -152,38 +95,3 @@ def install_dependencies(packages, start_time):
         else:
             os.system(f"apt install {package} > /dev/null 2>&1")
     print("\x1b[0m")
-
-
-def main(argv=sys.argv[1:]):
-    # Log datetime
-    start_time = time.time()
-    env_id = "colab" if is_colab else "mock"
-
-    # Get the bulkhours basic package
-    print("RUN git clone https://github.com/guydegnol/bulkhours.git [%s, %.0fs]" % (env_id, time.time() - start_time))
-
-    if is_colab:
-        os.system(
-            f"cd {bulk_dir} && rm -rf bulkhours 2> /dev/null && git clone https://github.com/guydegnol/bulkhours.git --depth 1 > /dev/null 2>&1"
-        )
-
-    if not os.path.exists(f"{bulk_dir}/bulkhours/"):
-        print("RUN install bulkhours: failed 💥")
-        return
-
-    args = get_install_parser(argv)
-
-    # Install main package
-    install_pkg("admin", is_colab, args.tokens, env_id, start_time, args.debug)
-    install_pkg("premium", is_colab, args.tokens, env_id, start_time, args.debug)
-    install_dependencies(args.packages, start_time)
-
-    # Dump env variables
-    with open(f"{bulk_dir}/bulkhours/.safep", "w", encoding="utf-8") as f:
-        json.dump(args.tokens, f, ensure_ascii=False, indent=4)
-        if args.debug:
-            print(args.tokens)
-
-
-if __name__ == "__main__":
-    main()
