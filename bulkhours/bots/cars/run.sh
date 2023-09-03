5ae5d06a82673ea6e03bb09d863b8ddb45a40b9b
guydegnol <gt@guydegnol.net>
2023-09-03 12:37:27 +0200

    Some changes

--- bulkhours/bots/cars/run.sh
+++ bulkhours/bots/cars/run.sh
@@ -1,11 +1,13 @@
-
-
+ #bash /home/pi/bulkhours/bulkhours/bots/cars/run.sh
 
 cp -f /home/pi/bulkhours/bulkhours/bots/cars/main.py /opt/ezblock/main_py.py
 cp -f /home/pi/bulkhours/bulkhours/bots/cars/app.py /opt/ezblock/streamlit_app.py
 
 # Copy info to the main directory
-# cp -f /home/pi/ezb-pi/workspace/*.py /opt/ezblock/*.py 
+#cp -f /home/pi/ezb-pi/workspace/*.py /opt/ezblock/*.py 
+cp -f /home/pi/ezb-pi/workspace/vilib.py /opt/ezblock/vilib.py 
+cp -f /home/pi/ezb-pi/workspace/picarx.py /opt/ezblock/picarx.py 
+
 
 cd /opt/ezblock/
 sudo python3 /opt/ezblock/main_py.py
