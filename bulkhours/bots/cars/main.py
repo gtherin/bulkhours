56970c473aba1fa07d39b5c3e68ad73e81db60e9
guydegnol <gt@guydegnol.net>
2023-09-04 16:32:11 +0200

    Update data

--- bulkhours/bots/cars/main.py
+++ bulkhours/bots/cars/main.py
@@ -4,82 +4,66 @@
 import os
 os.environ['PICARX_ROGUE_MODE'] = "True"
 
-from Music import *
-from ezblock import TTS
+import Music as music
 from vilib import Vilib
-from ezblock import Remote
-from picarx import PiCarX
-#import picarx
-#px = picarx.PiCarX()
-from ezblock import mapping
+import ezblock
+
+
+import picar
+import picappx
+px = picappx.px
 import cv2
 import pygame
+from sensor_hat.joystick_module import Joystick_Module
 
-#os.environ["SDL_VIDEODRIVER"] = "dummy"
-
-
-panAngle = None
-threadingA = None
-Ref1 = None
-Ref2 = None
-x = None
-
-tts = TTS()
-tts.lang('fr-FR')
-
-
+jy = Joystick_Module()
 
 def main():
 
-    music_set_volume(100)
-    tts.say('Bienvenue Sylvia')
+    if 0:
+        pin_D0=ezblock.Pin("D0")
+        pin_D1=ezblock.Pin("D1")
 
-    Vilib.camera_start(True)
-    #picarx.start_px_server()
+        distance = ezblock.Ultrasonic(pin_D0, pin_D1).read()
+        print("%s"%distance)
+        return
+
+    if 1:
+        picappx.start_px_server()
+
+    if 1:
+        Vilib.camera_start(True)
+        Vilib.human_detect_switch(True)
+        #Vilib.detect_color_name('red')
+
+    __RM_OBJECT__ = ezblock.Remote()
+
 
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
           print("YOYO")
+    print("AAAAAAAAAAAAA")
     return
 
-    background_music('come_as_you_are.mp3')
-    Vilib.human_detect_switch(True)
-    Vilib.detect_color_name('blue')
     Ref1 = 30
     Ref2 = 10
     panAngle = 0
 
-    __RM_OBJECT__ = Remote()
-
-    def play_music():
-        global panAngle, threadingA, Ref1, Ref2, x
-        music_set_volume(__RM_OBJECT__.get_slider_value("A"))
-
     def move_joystick():
-      global panAngle, threadingA, Ref1, Ref2, x
-      px.forward(__RM_OBJECT__.get_joystick_value("B", "Y"))
-      px.set_steering_angle((mapping(__RM_OBJECT__.get_joystick_value("B", "X"), (-100), 100, (-45), 45)))
+        px.forward(__RM_OBJECT__.get_joystick_value("B", "Y"))
+        px.set_steering_angle((ezblock.mapping(__RM_OBJECT__.get_joystick_value("B", "X"), (-100), 100, (-45), 45)))
 
     def camera_with_joystick():
-      global panAngle, threadingA, Ref1, Ref2, x
-      px.set_camera_pan_angle((mapping(__RM_OBJECT__.get_joystick_value("A", "X"), (-100), 100, (-45), 45)))
-      px.set_camera_tilt_angle((mapping(__RM_OBJECT__.get_joystick_value("A", "Y"), (-100), 100, (-45), 45)))
+        px.set_camera_pan_angle((ezblock.mapping(__RM_OBJECT__.get_joystick_value("A", "X"), (-100), 100, (-45), 45)))
+        px.set_camera_tilt_angle((ezblock.mapping(__RM_OBJECT__.get_joystick_value("A", "Y"), (-100), 100, (-45), 45)))
 
     def camera_with_pad():
-      global panAngle, threadingA, Ref1, Ref2, x
-      if __RM_OBJECT__.get_dpad_value("A", "U") == x:
-        panAngle = (panAngle if isinstance(panAngle, int) else 0) + 1
-      px.set_camera_pan_angle(panAngle)
+        if __RM_OBJECT__.get_dpad_value("A", "U") == x:
+            panAngle = (panAngle if isinstance(panAngle, int) else 0) + 1
+        px.set_camera_pan_angle(panAngle)
 
-    def forever():
-      global panAngle, threadingA, Ref1, Ref2, x
-      move_joystick()
-      camera_with_joystick()
-      play_music()
-      if __RM_OBJECT__.get_button_value("A") == 1:
-        tts.lang('fr-FR')
-        tts.say('On se fait un épisode de')
-        tts.lang('en-GB')
-        tts.say('the last of us ?')
+        if __RM_OBJECT__.get_button_value("A") == 1:
+            tts.lang('en-GB')
+            tts.say('Is it cool')
 
 main()
--- bulkhours/bots/cars/run.sh
+++ bulkhours/bots/cars/run.sh
@@ -1,4 +1,4 @@
- #bash /home/pi/bulkhours/bulkhours/bots/cars/run.sh
+#bash /home/pi/bulkhours/bulkhours/bots/cars/run.sh
 
 cp -f /home/pi/bulkhours/bulkhours/bots/cars/main.py /opt/ezblock/main_py.py
 cp -f /home/pi/bulkhours/bulkhours/bots/cars/app.py /opt/ezblock/streamlit_app.py
@@ -7,6 +7,11 @@ cp -f /home/pi/bulkhours/bulkhours/bots/cars/app.py /opt/ezblock/streamlit_app.p
 #cp -f /home/pi/ezb-pi/workspace/*.py /opt/ezblock/*.py 
 cp -f /home/pi/ezb-pi/workspace/vilib.py /opt/ezblock/vilib.py 
 cp -f /home/pi/ezb-pi/workspace/picarx.py /opt/ezblock/picarx.py 
+cp -f /home/pi/ezb-pi/workspace/picar.py /opt/ezblock/picar.py 
+cp -f /home/pi/ezb-pi/workspace/picappx.py /opt/ezblock/picappx.py 
+cp -f /home/pi/ezb-pi/workspace/Music.py /opt/ezblock/Music.py 
+
+#cp -f /home/pi/sunfounder-controller/examples/picarx_control.py /opt/ezblock/picarx_control.py 
 
 
 cd /opt/ezblock/
