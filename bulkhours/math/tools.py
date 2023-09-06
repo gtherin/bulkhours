79627209696ed8676bb1fe5608d607d5373ea436
guydegnol <bulkhours@guydegnol.net>
2023-09-06 16:39:57 +0200

    Add like.gif

---
+++ bulkhours/math/tools.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from fractions import Fraction
+
+
+def md(data, style="raw"):
+    import IPython
+
+    if style == "header":
+        data = r"<font size='+3'>%s</font>" % (data)
+
+    IPython.display.display(IPython.display.Markdown(r"%s" % data))
