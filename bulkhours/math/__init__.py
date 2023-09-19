b0f6ceeb3ae02ea826ec8e44b26541cc25089a77
guydegnol <gt@guydegnol.net>
2023-09-19 19:35:32 +0200

    fix from_vectir

--- bulkhours/math/__init__.py
+++ bulkhours/math/__init__.py
@@ -5,4 +5,9 @@ from .math_table import MathTable
 from . import td1
 from . import td2
 from .vector import Vector, VectorGrid, from_vector
-    
+
+def display_warning():
+    html("""🚧🚧🚧🚧<font color="red">Cette page est un brouillon de correction. 
+Il y a donc beaucoup encore d'erreurs. Merci de reporter les erreurs à guillaume.therin@ipsa.fr
+</font>🚧🚧🚧🚧<br/><br/>
+⚠️⚠️⚠️⚠️<b>ATTENTION DE BIEN EXECUTER LA PREMIERE CELLULE POUR QUE LA PAGE S'EXECUTE</b>⚠️⚠️⚠️⚠️""")
