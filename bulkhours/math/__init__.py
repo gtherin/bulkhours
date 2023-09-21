b49ad8fa2a3dfddd1c677deafd174fd941f01c66
guydegnol <gt@guydegnol.net>
2023-09-21 10:20:40 +0200

    Fix message

--- bulkhours/math/__init__.py
+++ bulkhours/math/__init__.py
@@ -8,17 +8,17 @@ from .vector import Vector, VectorGrid
 
 def display_warning(language = "fr"):
     if language == "fr":
-        text1 = 'Cette page utilise (<i>Package de support de cours interactif</i>). Si vous aimez cette approche🚀🏆🎯, <br/>vous pouvez supporter le projet sur <a href="https://github.com/guydegnol/bulkhours">github</a> en donnant quelques etoiles.'
+        text1 = 'Cette page utilise BulkHours 🚀🏆🎯 (<i>package de support de cours</i>). Si vous appreciez ces pages, <br/>vous pouvez supporter le projet sur <a href="https://github.com/guydegnol/bulkhours">github</a> en ajoutant des étoiles.'
     else:
         text1 = 'This page is using (<i>A Support course package</i>). If you like this approach🚀🏆🎯, <br/>please support the project on <a href="https://github.com/guydegnol/bulkhours">github</a> with some stars.'
 
     text2 = """🚧🚧🚧🚧<b>Attention!</b> Cette page est un <font color="red"><b> brouillon de correction</b></font>:🚧🚧🚧🚧<ol>
-<li><b>Il y a donc beaucoup encore d'erreurs</b>. Je n'ai pas encore eu le temps de tout corriger.
+  <li><b>Il y a donc beaucoup encore d'erreurs</b>. Je n'ai pas encore eu le temps de tout corriger.
 Merci beaucoup de reporter les erreurs à <A HREF="mailto:guillaume.therin@ipsa.fr">guillaume.therin@ipsa.fr</A></li>
-<li>Quelques exercices n'ont pas du tout de correction.</li>
-<li>La mise à jour doit encore etre ajustée.</li>
+  <li>Quelques exercices n'ont pas encore une correction complete.</li>
+  <li>La mise à jour doit encore etre ajustée.</li>
 </ol>
-⚠️Si vous voulez quand même continuer, allez dans l'onglet <b>"Execution"</b> et cliquez sur <b>"Redémarrer et tout exécuter"</b>⚠️
+⚠️Si vous voulez quand même continuer, allez dans l'onglet <b>"Execution"</b> et cliquez sur <b>"Redémarrer et tout exécuter"</b>, avant de pouvoir parcourir cette page.⚠️
 """
 
     html(
