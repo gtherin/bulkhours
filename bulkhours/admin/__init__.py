1022c50e03d35c5d17309735eddd89469dbb54fc
guydegnol <gt@guydegnol.net>
2023-10-11 17:03:00 +0200

    Prepare mails

--- bulkhours/admin/__init__.py
+++ bulkhours/admin/__init__.py
@@ -3,7 +3,7 @@ from . import answers  # noqa
 from . import tools  # noqa
 from .summary import summary  # noqa
 from . import dashboard  # noqa
-from .mail import send_mail  # noqa
+from .mail import send_mail, prepare_mail  # noqa
 
 # from .invoices.invoice import Invoice  # noqa
 from .audio import get_audio  # noqa
