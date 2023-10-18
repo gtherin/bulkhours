248c088f70fe396f0d38978426cad4285c9bcc4f
guydegnol <gt@guydegnol.net>
2023-10-18 06:59:04 +0200

    Adapt code to xtra functions

--- tests/test_cell_generation.py
+++ tests/test_cell_generation.py
@@ -5,48 +5,44 @@ data = """%%evaluation_cell_id -i synthetic
 
 def generate_data(n=40):
     np.random.seed(1)
-    df = pd.DataFrame(index=pd.date_range(start="2022", periods=n))
-    df["is_test"] = df.index >= df.index[-15]
+
     # 1. Create a noise with an expected drift of 3 and a expected std of 0.3
     df["noise"] = sp.stats.norm(loc=3, scale=0.3).rvs(n) # BKRESET.INIT:0
+
     # 2. Create a trend with an expected increment of 1.7 and a expected std of 0.9
     df["trend"] = sp.stats.norm(loc=1.7, scale=0.9).rvs(n).cumsum() # BKRESET.INIT:0
-
-    # 3. Add a seasonal signal
-    df["seasonal"] = [0, 12, 0, 0]*int(n/4)
 
     # 4. Add a column with the sum of the trend and seasonal
     df["trend+seasonal"] = df["trend"] + df["seasonal"] # BKRESET.INIT:0
     return df
 
-# Generate data
 df = generate_data(n=40)
 
 fig, axes = plt.subplots(ncols=4, figsize=(15, 4))
 for i, variable in enumerate(["noise", "trend", "seasonal", "trend+seasonal"]):
-    display(variable)
     ax = axes[i]
     # 4. Create analyses for the SimpleExp, Holt and 2xHolt-Winter models (one with a damp parameter of 0.96)
-    # For each model, you should be able to compare the parameters.
     # BKRESET.REMOVE:START
-    plot_prediction(df, {
-        "Pred(1xExp)": exp_optimizer(df, variable),
-        "Pred(2xExp)": exp_optimizer(df, variable, trend="add"),
-        "Pred(3xExp)": exp_optimizer(df, variable, trend="add", seasonal="add", seasonal_periods=4),
-        "Pred(3xExp, damped)": exp_optimizer(df, variable, trend="add", seasonal="add", seasonal_periods=4, damping_trend=0.96)
-            }, variable, "Holt-Winters model", ax=ax)
+    a = 3
+    a = 4
     # BKRESET.REMOVE:END
+
+def student_evaluation_function():
+    a = 3
+    return bulkhours.is_equal(student.flops, max_score=3)
 
 # 5. Comment
 print("... COMMENT...")  # BKRESET.REPLACE:print("... COMMENT...")
+
+
 """
 
 def test_cell_reset():
-    code = bulkhours.admin.cell_reset(data.split("\n"))
+    code = bulkhours.admin.cell_reset(data)
 
 
 
 def test_cell_solution():
-    code = bulkhours.admin.cell_solution(data.split("\n"))
+    code = bulkhours.admin.cell_solution(data)
     if "BKRESET." in code:
         raise Exception("Should not be here")
