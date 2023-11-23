2b6cd15232674e5b166c41408cfbf1702ca7101a
guydegnol <gt@guydegnol.net>
2023-11-23 15:37:50 +0100

    Some changes

--- bulkhours/admin/answers.py
+++ bulkhours/admin/answers.py
@@ -8,11 +8,13 @@ from . import tools
 
 def get_cfilename(cfg, cell_id):
     return core.tools.abspath(
-        f"data/cache/{cfg.subject}/{cfg.virtual_room}/admin_{cfg.notebook_id}_{cell_id}.json", create_dir=True
+        f"data/cache/{cfg.subject}/{cfg.virtual_room}/admin_{cfg.notebook_id}_{cell_id}.json",
+        create_dir=True,
     )
 
+
 def get_cdata(cfg, cell_id):
-    if os.path.exists(filename:= get_cfilename(cfg, cell_id)):
+    if os.path.exists(filename := get_cfilename(cfg, cell_id)):
         with open(filename) as json_file:
             return json.load(json_file)
     return {}
@@ -28,7 +30,6 @@ def get_answers(cell_id, update_git=False, verbose=False, aliases={}):
 
     data = {}
     for answer in docs:
-
         # Find the right name if typo in the db name
         student_id = str(answer.id).replace(" ", "")
         if aliases is not None and student_id in aliases:
@@ -48,9 +49,11 @@ def get_answers(cell_id, update_git=False, verbose=False, aliases={}):
 
         data[student_id] = cdata[student_id]
 
-    with open(filename:= get_cfilename(cfg, cell_id), "w", encoding="utf-8") as f:
+    with open(filename := get_cfilename(cfg, cell_id), "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    tools.update_github(update_git, msg=f"Cache file ({filename}) of {cell_id}", verbose=verbose)
+    tools.update_github(
+        update_git, msg=f"Cache file ({filename}) of {cell_id}", verbose=verbose
+    )
 
     return data
 
@@ -60,11 +63,17 @@ def update_grades(cell_id, grades, grade_name):
 
     uptime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     for k in grades.index:
-        update_note_in_db(cell_id, grades["mail"][k], grades[cell_id + ".n"][k], uptime, cfg=cfg, grade_name=grade_name)
+        update_note_in_db(
+            cell_id,
+            grades["mail"][k],
+            grades[cell_id + ".n"][k],
+            uptime,
+            cfg=cfg,
+            grade_name=grade_name,
+        )
 
 
 def update_note_in_db(cell_id, user, grade, uptime, grade_name="grade", cfg=None):
-
     core.Grade.check_gradname_validity(grade_name)
 
     if not core.Grade.is_valid(grade):
@@ -73,13 +82,16 @@ def update_note_in_db(cell_id, user, grade, uptime, grade_name="grade", cfg=None
     info = {grade_name: grade, grade_name + "_upd": uptime}
 
     try:
-        return core.firebase.get_document(question=cell_id, user=user, cinfo=cfg).update(info)
+        return core.firebase.get_document(
+            question=cell_id, user=user, cinfo=cfg
+        ).update(info)
     except:
-        return core.firebase.get_document(question=cell_id, user=user, cinfo=cfg).set(info)
+        return core.firebase.get_document(question=cell_id, user=user, cinfo=cfg).set(
+            info
+        )
 
 
 def update_grade(cell_id, user, grade, verbose=True, grade_name="grade", comment=""):
-
     cfg = core.tools.get_config(is_new_format=True)
 
     # Get grades
@@ -104,9 +116,10 @@ def update_grade(cell_id, user, grade, verbose=True, grade_name="grade", comment
 
     if verbose:
         print(f"\x1b[35m\x1b[1m{cmd}\x1b[m")
-        if comment!="":
-            IPython.display.display(IPython.display.Markdown(f"* **{user}**: {comment}"))
-
+        if comment != "":
+            IPython.display.display(
+                IPython.display.Markdown(f"* **{user}**: {comment}")
+            )
 
     # Set grades
     data[user][grade_name] = grade
