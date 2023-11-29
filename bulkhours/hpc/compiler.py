6f855a1e330a553a54eb1f03a25e3f90aa8795d3
guydegnol <gt@guydegnol.net>
2023-11-29 21:13:46 +0100

    Remove comment

--- bulkhours/hpc/compiler.py
+++ bulkhours/hpc/compiler.py
@@ -88,7 +88,6 @@ class CCPPlugin(Magics):
                 with open(file_code, "w") as f:
                     f.write(params["main_execution"])
 
-                print(f"compiler {file_code} -o {file_path}.out")
                 # Compile file
                 if self.cinfo.compiler in ["g++", "gcc"]:
                     cmd = (
