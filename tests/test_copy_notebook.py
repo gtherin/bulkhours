2ed6b028079af76cd1be2013fde9b53dd95cf881
guydegnol <gt@guydegnol.net>
2023-11-30 18:34:20 +0100

    Update data

--- tests/test_copy_notebook.py
+++ tests/test_copy_notebook.py
@@ -3,7 +3,19 @@ import bulkhours
 
 
 def test_copy_notebook():
-
-    bulkhours.admin.mail.copy("yo.da@jedi.com", "examples/", "2_Course_Edition.ipynb", "john.doe@bulkhours.eu", reset=True, debug=False)
+    bulkhours.admin.mail.copy(
+        "yo.da@jedi.com",
+        "examples/",
+        "2_Course_Edition.ipynb",
+        "john.doe@bulkhours.eu",
+        reset=True,
+        debug=False,
+    )
 
     os.system("rm -rf examples/2_Course_Edition_3*.ipynb")
+
+
+def test_evaluate_notebook():
+    bulkhours.core.firebase.init_database({})
+
+    bulkhours.admin.nevaluate("tests/TP5_Music_generation_TISA.ipynb", fake=True)
