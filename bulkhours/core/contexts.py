8e47cb0fe0129959bf65b97ce98dbb5091091ca9
guydegnol <gt@guydegnol.net>
2023-12-13 11:06:20 +0100

    Debug

--- bulkhours/core/contexts.py
+++ bulkhours/core/contexts.py
@@ -111,15 +111,9 @@ class Cteacher:
         model = tf.keras.Sequential([
             tf.keras.layers.Flatten(input_shape=[12288]),
             tf.keras.layers.Dense(100, activation="relu", kernel_initializer="he_normal", name="layer1"),
-            # Insert 3 hidden layers of sizes 80, 20, 5, with relu, and kernel_initializer="he_normal" (≈3 lines)
-            tf.keras.layers.Dense(80, activation="relu", kernel_initializer="he_normal", name="layer2"),
-            tf.keras.layers.Dense(20, activation="relu", kernel_initializer="he_normal", name="layer3"),
-            tf.keras.layers.Dense(5, activation="relu", kernel_initializer="he_normal", name="layer4"),
             tf.keras.layers.Dense(1, activation="sigmoid", kernel_initializer="he_normal", name="layer5")])
         
         print("AAAAAAAAAAAAAAAAAAA 5")
-        for i in [2, 3, 4]:
-            print("model.layers[2].weights[0].shape=", np.array(model.layers[i].weights[0].shape))
                 
 teacher = Cteacher()
 
