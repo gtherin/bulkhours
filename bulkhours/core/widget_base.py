bee1420e49d262727958fc8e20aa784e777c9251
guydegnol <gt@guydegnol.net>
2024-01-08 12:31:12 +0100

    Remove dead parameter

--- bulkhours/core/widget_base.py
+++ bulkhours/core/widget_base.py
@@ -44,7 +44,7 @@ class WidgetBase:
 
         if teacher_data.is_evaluation_available():
             score = equals.student_evaluation_function(
-                student_data, teacher_data, raw=False, user=self.cinfo.user
+                student_data, teacher_data, user=self.cinfo.user
             ).score
         else:
             score = ""
