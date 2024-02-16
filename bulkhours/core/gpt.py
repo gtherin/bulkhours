a5c7ab73ab5034ff87697299046c1bc69a221199
bulk <no-reply@bulkhours.fr>
2024-02-16 18:06:45 +0000

    Better

--- bulkhours/core/gpt.py
+++ bulkhours/core/gpt.py
@@ -173,7 +173,7 @@ def ask_gpt(
                 IPython.display.display(IPython.display.Code(c))
 
 
-def get_grade(student_data, teacher_data):
+def get_grade(student_data, teacher_data, max_score):
 
     if (
         "main_execution" in student_data.minfo
@@ -184,7 +184,7 @@ def get_grade(student_data, teacher_data):
         prompt = f"""{evaluation_instructions}
 - question:\n<start>\n{teacher_data.get_reset()}\n</start>
 - actual solution:\n<end>\n{teacher_data.get_solution()}\n</end>
-- student's solution:\n<answer>\n{student_data.get_solution()}\n</answer>\n"""
+- student's solution:\n<answer>\n{student_data.get_solution()}\n</answer>\n""".replace("MAX_SCORE", max_score)
 
         # print(prompt)
         response = ask_gpt(
