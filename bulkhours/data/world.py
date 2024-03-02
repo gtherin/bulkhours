ac6acc3844ab74456ff2ca86ce8a249e19ca9990
bulk <no-reply@bulkhours.fr>
2024-03-02 11:19:52 +0000

    Update package

--- bulkhours/data/world.py
+++ bulkhours/data/world.py
@@ -9,6 +9,7 @@ def get_mapgeneric(df):
     if "continent" in df.columns:
         del df["continent"]
 
+    df = df.rename(columns={"Country": "country"})
     df["country"] = df["country"].str.replace("United States", "United States of America")
     df["country"] = df["country"].str.replace("Democratic Republic of Congo", "Dem. Rep. Congo")
 
