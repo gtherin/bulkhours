b6dd81990340088bdf4ea340b6a2d2db29bb7120
bulk <no-reply@bulkhours.fr>
2024-09-04 16:28:48 +0000

    Update data

--- bulkhours/data/vegetables.py
+++ bulkhours/data/vegetables.py
@@ -59,15 +59,15 @@ def download_kaggle_data(filename, chunck_size=40960):
     download_url = "https://storage.googleapis.com/kaggle-data-sets/1817999/2965251/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240903%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240903T114415Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=54863d9aab9113332f639e5f141b72361f8d184e885a1a77ad65d035fde5f9e5e2759b2cab289f8c22ed3befe0b3b28f88d1bf957f6d9d3dbd707101984359b488848d0fb3b896694a72a67b4a872d895288e0ea08d413754c1e6db69a64ad3a7d3c22e8a2d06a05515dcd107af66eee290ad42e485d345e18490c55836c0ff520eb460503aa666e2a12b00919f32baf72e2d14450d506a28c606b05711a8b8437aa1ddcacaf97059273b14c7a0c938049e0525508ff1d27bd53726217632cce253134558805700c5e8a83e359621960b73b6fc48f9e43fd834728c9510459514e9bb01b60ba659d34e75d6f3117e65a7e7e8570fba95b5efafc56f89b7fff15"
     dfilename = urlparse(download_url).path
     with urlopen(download_url) as fileres, NamedTemporaryFile() as tfile:
-        total_length = int(float(fileres.headers['content-length'])*1e-6)
-        print(f'\033[36mDownloading {destination_path}{filename} data ({total_length} MO compressed)\033[39m')
+        total_length = int(fileres.headers['content-length'])
+        print(f'\033[36mDownloading {destination_path}{filename} data ({total_length*1e-6:.0f} MO compressed)\033[39m')
         dl = 0
         data = fileres.read(chunck_size)
         while len(data) > 0:
             dl += len(data)
             tfile.write(data)
             done = int(50 * dl / total_length)
-            sys.stdout.write(f"\033[36m\r[{'=' * done}{' ' * (50-done)}] {dl} MO downloaded\033[39m")
+            sys.stdout.write(f"\033[36m\r[{'=' * done}{' ' * (50-done)}] {dl*1e-6:.0f} MO downloaded\033[39m")
             sys.stdout.flush()
             data = fileres.read(chunck_size)
         if dfilename.endswith('.zip'):
