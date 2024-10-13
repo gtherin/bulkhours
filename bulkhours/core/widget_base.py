6865536007ef60814fd98c74a7123938f16bfcd3
bulk <no-reply@bulkhours.fr>
2024-10-13 23:04:42 +0000

    Better

--- bulkhours/core/widget_base.py
+++ bulkhours/core/widget_base.py
@@ -182,10 +182,10 @@ class WidgetBase:
                 cinfo=self.cinfo, user=self.user, data=self.cell_source, output=output
             )
             print(self.cinfo)
+            print(self.cinfo.virtual_rooms)
             virtual_rooms = [self.cinfo.virtual_room] if virtual_rooms == "" else self.cinfo.virtual_rooms.split(",")
 
             print(virtual_rooms)
-            print(self.cinfo.virtual_rooms)
             virtual_rooms = ["toy", "TISA"]
 
             for virtual_room in virtual_rooms:
