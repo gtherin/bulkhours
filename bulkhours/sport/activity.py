d416b5a2fa4301a367255ab5d098f40174403124
bulk <no-reply@bulkhours.fr>
2026-02-06 18:12:53 +0000

    Better

--- bulkhours/sport/activity.py
+++ bulkhours/sport/activity.py
@@ -3,38 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
-
-
-def read_from_drive(folder_name, activity):
-    import requests
-    # Download file bytes
-    response = requests.get(f"https://drive.google.com/uc?export=download&id={activity.id}")
-    data = response.content
-    return data
-
-
-def read_fit_file(data):
-    import gzip
-    import io
-    from fitparse import FitFile
-
-    # If file is gzipped, decompress it
-    fit_bytes = gzip.decompress(data)
-
-    # Load FIT
-    fitfile = FitFile(io.BytesIO(fit_bytes))
-
-    # Get all data messages that are of type 'record'
-    records = []
-    for record in fitfile.get_messages('record'):
-        # Get all data fields from the record
-        record_data = {}
-        for data_field in record.fields:
-            record_data[data_field.name] = data_field.value
-        records.append(record_data)
-
-    # Create a pandas DataFrame
-    return pd.DataFrame(records)
+import requests
 
 
 class Activity:
@@ -42,18 +11,113 @@ class Activity:
         self.atype = atype
         self.info = info
 
-        if local:
-            with open(f'/home/ubuntu/bulkcats/strava/{self.info.filename}', "rb") as f:
-                data = f.read()
-        else:
-            data = read_from_drive(folder_name, activity)
-
-        if '.fit' in self.info.filename:
-            df = read_fit_file(data)
-            #df = read_fit_file(folder_name, self.info)
-            df = df[[c for c in df.columns if not c.startswith('unknown_')]].set_index('timestamp')
-            df["minutes"] = (df.index - df.index[0]).total_seconds() / 60.
-            df["heart_rate"] = df["heart_rate"].replace(0, np.nan)
+        try:
+            if '.fit' in self.info.filename:
+                if local:
+                    with open(f'/home/ubuntu/bulkcats/strava/{self.info.filename}', 'rb') as f:
+                        data = f.read()
+                else:
+                    response = requests.get(f"https://drive.google.com/uc?export=download&id={self.info.id}")
+                    data = response.content
+                df = Activity.read_fit_file(data)
+            elif '.gpx' in self.info.filename:
+                df = Activity.read_gpx_file(f'/home/ubuntu/bulkcats/strava/{self.info.filename}')
+            elif '.tcx' in self.info.filename:
+                df = Activity.read_tcx_file(f'/home/ubuntu/bulkcats/strava/{self.info.filename}')
+            else:
+                print(f'Fuck {self.info.filename}')
+                raise Exception(f'Fuck {self.info.filename}')
+            df.index = pd.to_datetime(df.index)
+            df["seconds"] = (df.index - df.index[0]).total_seconds()
+            df["minutes"] = df["seconds"] / 60.
+            df["delta_seconds"] = df["seconds"].diff()
+            if 'heart_rate' in df.columns:
+                df["heart_rate"] = df["heart_rate"].replace(0, np.nan)
+            else:
+                df["heart_rate"] = np.nan
             self.df = df
-        else:
-            error
+        except:
+            self.df = pd.DataFrame()
+
+    @staticmethod
+    def read_fit_file(data):
+        import gzip
+        import io
+        from fitparse import FitFile
+
+        # If file is gzipped, decompress it
+        fit_bytes = gzip.decompress(data)
+
+        # Load FIT
+        fitfile = FitFile(io.BytesIO(fit_bytes))
+
+        # Get all data messages that are of type 'record'
+        records = []
+        for record in fitfile.get_messages('record'):
+            # Get all data fields from the record
+            record_data = {}
+            for data_field in record.fields:
+                record_data[data_field.name] = data_field.value
+            records.append(record_data)
+
+        # Create a pandas DataFrame
+        df = pd.DataFrame(records)
+        return df[[c for c in df.columns if not c.startswith('unknown_')]].set_index('timestamp')
+
+    @staticmethod
+    def read_gpx_file(path):
+        import gpxpy
+        rows = []
+        with open(path, "r") as f:
+            gpx = gpxpy.parse(f)
+
+        for track in gpx.tracks:
+            for seg in track.segments:
+                for p in seg.points:
+                    row = {"time": p.time, "lat": p.latitude, "lon": p.longitude, "elevation": p.elevation}
+                    # ---- EXTENSIONS (HR, cadence, power, temp...) ----
+                    for ext in p.extensions:
+                        for child in ext:
+                            tag = child.tag.split("}")[-1]  # remove namespace
+                            try:
+                                row[tag] = float(child.text)
+                            except (TypeError, ValueError):
+                                row[tag] = child.text
+
+                    rows.append(row)
+        return pd.DataFrame(rows).set_index("time")
+
+    @staticmethod
+    def read_tcx_file(path):
+
+        import gzip
+        import pandas as pd
+        import xmltodict
+
+        with gzip.open(path, "rb") as f:
+            tcx = xmltodict.parse(f)
+
+        # Navigate to trackpoints (handles 1 or multiple laps)
+        activity = tcx["TrainingCenterDatabase"]["Activities"]["Activity"]
+        laps = activity["Lap"]
+
+        if not isinstance(laps, list):
+            laps = [laps]
+
+        trackpoints = []
+        for lap in laps:
+            track = lap.get("Track")
+            if not track:
+                continue
+            tps = track["Trackpoint"]
+            if isinstance(tps, dict):
+                tps = [tps]
+            trackpoints.extend(tps)
+
+        df = pd.json_normalize(trackpoints).set_index('Time')
+        df = df.rename(columns={
+            'DistanceMeters': 'distance',
+            'AltitudeMeters': 'altitude',
+            })
+        print(df)
+        return df
--- bulkhours/sport/strava.py
+++ bulkhours/sport/strava.py
@@ -1,242 +1,84 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys
+
+from .activity import Activity
+from .activities import Activities
+from .athlete import Athlete
+from ..data import safe_loader as loader
 
 
-def format_activities(df) -> pd.DataFrame:
-    """Clean and compact Strava French export column names."""
+class Performance:
 
-    from pathlib import Path
-    import gzip
-    from fitparse import FitFile
-    import re
-    import dateparser
+    def __init__(self, athlete, activities):
+        self.athlete = athlete
+        self.activities = activities
 
-    mapping = {
-        "ID de l'activité": "activity_id",
-        "Date de l'activité": "date",
-        "Nom de l'activité": "name",
-        "Nom du fichier": "filename",
-        "Type d'activité": "type",
-        "Temps écoulé": "elapsed_s",
-        "Temps écoulé.1": "elapsed_s1",
-        "Durée de déplacement": "moving_s",
-        "Distance": "distance_km",
-        "Distance.1": "distance_m",
-        "Vitesse max.": "vmax",
-        "Vitesse moyenne": "vmean",
-        "Dénivelé positif": "elev_gain",
-        "Dénivelé négatif": "elev_loss",
-        "Altitude min.": "alt_min",
-        "Altitude max.": "alt_max",
-        "Pente max.": "grade_max",
-        "Pente moyenne": "grade_mean",
-        "Cadence max.": "cad_max",
-        "Cadence moyenne": "cad_mean",
-        "Puissance moyenne": "pwr_mean",
-        "Puissance moyenne pondérée": "pwr_weighted",
-        "Calories": "calories",
-        "Charge d’entraînement": "load",
-        "Intensité": "intensity",
-        "Récupération": "recovery",
-        "Support": "media_path",
-        "Poids du vélo": "bike_weight",
-        "À partir du téléchargement": "from_upload",
-        "Distance sur chemin": "trail_distance",
-        "CO2 économisé": "co2_saved",
-        'Fréquence cardiaque max.': 'max_hr',
-        'Fréquence cardiaque max..1': 'max_hr1',
-        'Fréquence cardiaque moyenne': 'avg_hr',
-        'Vitesse moyenne ajustée selon la pente': 'vmean_adjslope',
-    }
+    def preprocess(self):
+        gdf = self.activities.df
+        gdf['zones'] = ''
+        zone_in_bpm = self.athlete.get_zones_in_bpm()
+        print(self.athlete.zones, zone_in_bpm)
+        gdf['TRIMP'] = self.athlete.trimp_from_df(gdf)
+        return
 
-    columns = set(['Effort relatif', 'Température moyenne', 'Effort relatif.1', 'Puissance moyenne pondérée',
-       'Distance ajustée selon la pente', 'Intensité des précipitations', 'Couverture nuageuse', 'Signalé', 'Vitesse moyenne (temps écoulé)',
-       'Longueur de piscine'] + list(mapping.keys()))
-    df = df[list(columns)]
-    df = df.rename(columns=mapping)
+        strimps = {
+            16731673287: 140,
+            16779135626: 77,
+            16802972475: 91,
+            16854653997: 92,
+            16915566584: 215,
+            16935972434: 118,
+            17012903551: 177, # 2025-01-11
+            17049890266: 145, # 2025-01-14
+            17061165277: 161, # 2025-01-15
+            }
+        gdf['STRIMP'] = gdf['activity_id'].map(strimps)
 
-    # Clean the rest (remove accents, punctuation, spaces)
-    def clean_col(c):
-        c = c.lower().replace("’", "'").replace("'", "").replace("î", "i").replace("é", "e").replace("è", "e").replace("à", "a").replace("ç", "c")
-        c = re.sub(r"[^a-z0-9_]+", "_", c)
-        c = re.sub(r"_+", "_", c).strip("_")
-        return c
+        for index in gdf.index:
 
-    df.columns = [clean_col(col) for col in df.columns]
-    arename = {k: clean_col(k) for k in list(df['type'].unique())}
-    df['type'] = df['type'].map(arename)
+            print(index, gdf.loc[index]['date'], gdf.loc[index]['activity_id'], gdf.loc[index]['type'], gdf.loc[index]['filename'])
+            activity = Activity(None, gdf.loc[index], local=True)
+            if activity.df.empty:
+                continue
 
-    num_cols = ["distance_km", "vmean", "vmean_adjslope", "avg_hr",  "max_hr", "calories", "moving_s"]
-    for col in num_cols:
-        if col in df.columns:
-            df[col] = (df[col].astype(str).str.replace(",", ".").astype(float, errors="ignore"))
+            df = activity.df#[['heart_rate']]
+            df = df.set_index("seconds")
 
-    return df
+            tzones = {}
+            rzones = []
+            for i in range(5):
+                ev = df[(df['heart_rate'] >= zone_in_bpm[i]) & (df['heart_rate'] < zone_in_bpm[i+1])]
+                rzones.append(f"{ev['delta_seconds'].sum():.0f}")
+
+            gdf.loc[index, 'zones'] = ';'.join(rzones)
+            print(rzones)
+
+        print(gdf.columns)
+        print(gdf[['load', 'activity_id', 'elapsed_s', 'type', 'avg_hr', 'date','zones', 'TRIMP', 'STRIMP']].tail(10))
+        print(f"/home/ubuntu/bulkcats/strava/summary.csv")
+        loader.secure_save(gdf, f"/home/ubuntu/bulkhours/data/stravism.csv.enc", os.environ['BULK_SPORT_KEY'])
+        gdf.to_csv(filename:=f"/home/ubuntu/bulkcats/strava/summary.csv")
+        self.activities.df = df
 
 
-def read_fit_file(folder_name, activity):
+def preprocess(argv=sys.argv):
+    file_id = os.environ['BULK_SPORT_ACTIVITIES_FOLDER']
+    activities = Activities(folder_name='/home/ubuntu/bulkcats/strava')
 
-    import requests
-    import gzip
-    import io
-    from fitparse import FitFile
+    athlete = Athlete(age=46, weight=79, hr_max=174, vo2_max=52,
+                      hr_rest=48, v_max=27, css=0.86, ftp=210)
 
-    url = f"https://drive.google.com/uc?export=download&id={activity.id}"
+    p = Performance(athlete, activities)
+    p.preprocess()
 
-    # Download file bytes
-    response = requests.get(url)
-    data = response.content
-
-    # If file is gzipped, decompress it
-    fit_bytes = gzip.decompress(data)
-
-    # Load FIT
-    fitfile = FitFile(io.BytesIO(fit_bytes))
-
-    # Get all data messages that are of type 'record'
-    records = []
-    for record in fitfile.get_messages('record'):
-        # Get all data fields from the record
-        record_data = {}
-        for data_field in record.fields:
-            record_data[data_field.name] = data_field.value
-        records.append(record_data)
-
-    # Create a pandas DataFrame
-    return pd.DataFrame(records)
-
-
-class Activity:
-    def __init__(self, folder_name, info, atype=None, *args, **kwargs):
-        self.atype = atype
-        self.info = info
-        if '.fit' in self.info.filename:
-            df = read_fit_file(folder_name, self.info)
-            df = df[[c for c in df.columns if not c.startswith('unknown_')]].set_index('timestamp')
-            df["minutes"] = (df.index - df.index[0]).total_seconds() / 60.
-            df["heart_rate"] = df["heart_rate"].replace(0, np.nan)
-            self.df = df
-        else:
-            error
-
-
-class Activities:
-
-    def __init__(self, folder_name=None, summary_file=None, activities_folder_info=None):
-        self.folder_name, self.summary_file, self.activities_folder_info = folder_name, summary_file, activities_folder_info
-
-        if folder_name is not None:
-            self.df = pd.read_csv(f"{folder_name}/activities.csv")
-        else:
-            self.df = pd.read_csv(f"https://drive.google.com/uc?export=download&id={self.summary_file}")
-
-        self.df = format_activities(self.df)
-        self.format_date()
-        self.df = self.df.merge(self.read_list(), how='left', on="filename")
-
-    def read_list(self):
-        alist = pd.read_json(f"https://drive.google.com/uc?export=download&id={self.activities_folder_info}").T
-        alist['filename'] = 'activities/' + alist.index
-        return alist
-
-    def format_date(self):
-        months = {"janv.": "Jan", "févr.": "Feb", "mars": "Mar", "avr.": "Apr", "mai": "May", "juin": "Jun", 
-        "juil.": "Jul", "août": "Aug", "sept.": "Sep", "oct.": "Oct", "nov.": "Nov", "déc.": "Dec"}
-
-        s = self.df["date"]
-        for fr, en in months.items():
-            s = s.str.replace(fr, en, regex=False)
-        self.df["date"] = pd.to_datetime(s, format="%d %b %Y, %H:%M:%S")
-
-    def get_activity(self, index, atype=None):
-        # ['course_a_pied', 'natation', 'velo', 'randonnee', 'velo_virtuel', 'kayak', 'entra_nement', 'stand_up_paddle']
-        if atype is not None:
-            activity = self.df[self.df['type'] == atype].iloc[index].dropna()
-        else:
-            activity = self.df.iloc[index].dropna()
-        return Activity(self.folder_name, activity, atype)
-
-
-def get_synthetic_biking_data(m=500):
-    df_bike = pd.DataFrame({
-        "weekly_tss": np.random.normal(420, 130, m).clip(80, 900),
-        "hrs_week": np.random.normal(7, 3, m).clip(2, 20),
-        "z3_pct": np.random.normal(15, 5, m).clip(5, 35),
-        "z4_pct": np.random.normal(9, 4, m).clip(2, 25),
-        "z5_pct": np.random.normal(2, 1, m).clip(0.1, 8),
-        "weight": np.random.normal(72, 7, m),})
-
-    df_bike["ftp"] = (
-        0.8 * df_bike["weekly_tss"]
-        + 2.4 * df_bike["z4_pct"]
-        + np.random.normal(0, 30, m)
-    ) / 10 + 50
-    return df_bike
-
-def get_synthetic_running_data(n=600):
-
-    np.random.seed(42)
-
-    n = 600
-
-    df_run = pd.DataFrame({
-        "avg_hr": np.random.normal(150, 9, n),
-        "weekly_km": np.random.normal(50, 14, n).clip(10, 130),
-        "weight": np.random.normal(70, 7, n),
-        "vo2max_est": np.random.normal(48, 4, n),
-    })
-
-    df_run["time_10k"] = (
-        60
-        - 0.32 * (df_run["weekly_km"] - 40)
-        - 0.6 * (df_run["vo2max_est"] - 45)
-        + 0.06 * (df_run["weight"] - 70)
-        + np.random.normal(0, 1.5, n)
-    )
-    return df_run
-
-def get_vo2max_synthetic_data(n=200, random_state=42):
-    np.random.seed(random_state)
-
-    speed_5min = np.random.normal(15, 2, n)
-    hr_max = np.random.normal(185, 8, n)
-    age = np.random.normal(35, 10, n)
-    weight = np.random.normal(72, 10, n)   # <<< nouvelle variable
-
-    # VO2max synthétique (relation linéaire + bruit)
-    vo2max = 3.5 * speed_5min + 0.15 * hr_max - 0.2 * age - 0.1 * weight + np.random.normal(0, 3, n)
-
-    return pd.DataFrame({
-        "speed_5min": speed_5min,
-        "hr_max": hr_max,
-        "age": age,
-        "weight": weight,
-        "vo2max": vo2max
-    })
-
-def get_pca_synthetic_data(n=200, random_state=42):
-    rng = np.random.default_rng(0)
-    n = 300
-
-    # ----- MATRICE DE CORRÉLATION RÉALISTE -----
-    corr = np.array([
-        [1.00,  0.80, -0.65,  0.20,  0.85],
-        [0.80,  1.00, -0.55,  0.30,  0.75],
-        [-0.65, -0.55, 1.00, -0.20, -0.45],
-        [0.20,  0.30, -0.20,  1.00,  0.25],
-        [0.85,  0.75, -0.45,  0.25,  1.00]
-    ])
-    L = np.linalg.cholesky(corr)
-    Z = rng.normal(size=(n, 5))
-    X = Z @ L.T
-
-    # ----- DONNÉES RÉALISTES-----
-    df = pd.DataFrame({
-        "vo2max":    X[:,0] * 7 + 55,
-        "power_w":   X[:,1] * 40 + 260,
-        "heartrate": X[:,2] * 8 + 150,
-        "cadence":   X[:,3] * 4 + 88,
-        "speed_5km": X[:,4] * 1.2 + 16
-    })
-    return df
+    return
+    if list_files:
+        import bulkback
+        bulkback.admin.gdrive.GFolders.force_list_folder(
+            file_id, 
+            #folder_cache=f"/home/ubuntu/bulkcats/strava/folder.{file_id}.json"
+            folder_cache=f"/home/ubuntu/bulkcats/strava/activities_folder_info.json"
+            )
